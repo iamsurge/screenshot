b89ed14f5c668fe428c7c5dc3fd064c9c2cc40cb
iamsurge <112551585+iamsurge@users.noreply.github.com>
2022-10-10 21:55:06 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -1,15 +1,16 @@
 const puppeteer = require('puppeteer');
-const { readFileSync, writeFileSync } = require("fs")
+const { readFileSync, writeFileSync, readdirSync } = require("fs");
+const sites = readdirSync('./datas').filter(file => file.endsWith('.png'));
 
-async function browser(url) {
+async function browser() {
     const browser = await puppeteer.launch();
+    
+ for (const file of sites) {   
     const page = await browser.newPage();
-
-    await page.goto(url, {waitUntil: 'networkidle0'});
-    await page.waitFor(10000);
-    writeFileSync("./output.txt", await page.content());
-    await page.screenshot({path: './screenshot.png', fullPage: true});
+    await page.goto(`https://${files.slice(4)}`, {waitUntil: 'networkidle0'});
+    await page.screenshot({path: `./datas/${file}`, fullPage: true});
+   }
     await browser.close();
 }
 
-browser("https://joelee.works/");
+browser();
