dda762f81bdedb7d6208ad0fa4f3cafcce9bb58b
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-10 22:11:45 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -6,7 +6,7 @@ const sites = readdirSync('./datas').filter(file => file.endsWith('.png'));
 sites.forEach(async (file) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`https://${file.slice(4)}`, {waitUntil: 'networkidle0'});
+    await page.goto(`https://${file.slice(0, -4)}`, {waitUntil: 'networkidle0'});
     await page.screenshot({path: `./datas/${file}`, fullPage: true});
     await browser.close();
 })
