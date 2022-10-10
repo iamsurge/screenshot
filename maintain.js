c9afca6d106559267b56d7a6be5545924a8a0b80
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2022-10-10 22:25:38 +0800

    Update maintain.js

--- maintain.js
+++ maintain.js
@@ -7,7 +7,7 @@ sites.forEach(async (file) => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(`https://${file.slice(0, -4).split("@").join("/")}`, {waitUntil: 'networkidle0'});
-    await page.waitFor(5000);
+    await page.waitFor(15000);
     await page.screenshot({path: `./datas/${file}`, fullPage: true});
     await browser.close();
 })
